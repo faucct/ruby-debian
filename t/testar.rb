dfd747d458f86f6a6cf007bfe9399a421a14eb02
Laurent Arnoud <laurent@spkdev.net>
2014-02-07 14:41:35 +0100

    Fix t/testar.rb for ruby2.0

--- t/testar.rb
+++ t/testar.rb
@@ -1,36 +1,34 @@
-require 'runit/testcase'
-require 'runit/cui/testrunner'
+require 'minitest/autorun'
 
-$:.unshift("../lib")
-require 'debian/ar'
+require_relative '../lib/debian/ar'
 
-class TestDebian__Ar < RUNIT::TestCase
+class TestDebian__Ar < MiniTest::Test
 
   def setup
-    @ruby = Dir["/var/cache/apt/archives/ruby_1.6*.deb"]
+    @ruby = Dir["/var/cache/apt/archives/ruby2.0_*.deb"]
     if @ruby.empty?
-      assert_fail("no ruby package in /var/cache/apt/archives")
+      flunk("no ruby package in /var/cache/apt/archives")
     end
   end
   def test_list
     @ruby.each {|deb|
-      assert_equals(['debian-binary','control.tar.gz','data.tar.gz'],
-		   Debian::Ar.new(deb).list.collect {|arf| arf.name })
+      assert_equal(['debian-binary','control.tar.gz','data.tar.xz'],
+                   Debian::Ar.new(deb).list.collect {|arf| arf.name })
     }
   end
 
   def test_each_file
     @ruby.each {|deb|
-      lists = ['debian-binary','control.tar.gz','data.tar.gz']
+      lists = ['debian-binary','control.tar.gz','data.tar.xz']
       Debian::Ar.new(deb).each_file {|name,io|
-	assert_equals(lists[0], name)
-	if name == 'debian-binary'
-	  assert_equals("2.0\n", io.read)
-	end
-	assert_equals(0, io.stat.uid)
-	assert_equals(0, io.stat.gid)
-	assert_equals(0100644, io.stat.mode)
-	lists.shift
+        assert_equal(lists[0], name)
+        if name == 'debian-binary'
+          assert_equal("2.0\n", io.read)
+        end
+        assert_equal(0, io.stat.uid)
+        assert_equal(0, io.stat.gid)
+        assert_equal(0100644, io.stat.mode)
+        lists.shift
       }
     }
   end
@@ -38,22 +36,10 @@ class TestDebian__Ar < RUNIT::TestCase
   def test_open
     @ruby.each {|deb|
       ar = Debian::Ar.new(deb)
-      assert_not_nil(ar.open("debian-binary"))
-      assert_equals("2.0\n", ar.open("debian-binary").read)
-      assert_not_nil(ar.open("control.tar.gz"))
-      assert_not_nil(ar.open("data.tar.gz"))
+      assert_instance_of Debian::Ar::ArFile, ar.open("debian-binary")
+      assert_equal("2.0\n", ar.open("debian-binary").read)
+      assert_instance_of Debian::Ar::ArFile, ar.open("control.tar.gz")
+      assert_instance_of Array, ar.open("data.tar.gz")
     }
   end
-end
-
-if $0 == __FILE__
-  if ARGV.size == 0
-    suite = TestDebian__Ar.suite
-  else
-    suite = RUNIT::TestSuite.new
-    ARGV.each do |testmethod|
-      suite.add_test(TestDebian__Ar.new(testmethod))
-    end
-  end
-  RUNIT::CUI::TestRunner.run(suite)
 end
